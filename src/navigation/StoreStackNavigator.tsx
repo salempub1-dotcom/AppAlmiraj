21ad9f40a359a42b415dc3409b00f4943c061a6e
salempub1-dotcom <salempub1@gmail.com>
2026-08-30 20:30:29 +0100

    feat: add checkout and order history navigation

--- src/navigation/StoreStackNavigator.tsx
+++ src/navigation/StoreStackNavigator.tsx
@@ -1,5 +1,8 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { CartScreen } from '../features/store/screens/CartScreen';
+import { CheckoutScreen } from '../features/store/screens/CheckoutScreen';
+import { MyOrdersScreen } from '../features/store/screens/MyOrdersScreen';
+import { OrderDetailScreen } from '../features/store/screens/OrderDetailScreen';
 import { ProductDetailScreen } from '../features/store/screens/ProductDetailScreen';
 import { StoreScreen } from '../features/store/screens/StoreScreen';
 import { useTheme } from '../context/ThemeProvider';
@@ -22,6 +25,9 @@ export function StoreStackNavigator() {
       <Stack.Screen name="StoreHome" component={StoreScreen} options={{ headerShown: false }} />
       <Stack.Screen name="ProductDetail" component={ProductDetailScreen} options={{ title: 'تفاصيل المنتج' }} />
       <Stack.Screen name="Cart" component={CartScreen} options={{ title: 'سلة الطلب' }} />
+      <Stack.Screen name="Checkout" component={CheckoutScreen} options={{ title: 'إتمام الطلب' }} />
+      <Stack.Screen name="MyOrders" component={MyOrdersScreen} options={{ title: 'طلباتي' }} />
+      <Stack.Screen name="OrderDetail" component={OrderDetailScreen} options={{ title: 'تفاصيل الطلب' }} />
     </Stack.Navigator>
   );
 }
